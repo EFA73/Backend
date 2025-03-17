aa7dacd94de9f56d5699354efc47021a5562beb3
yyysolhhh <solbaram37@gmail.com>
2025-03-17 17:27:52 +0900

    feat: 프로젝트 생성

---
+++ src/test/java/com/efa73/trackee/TrackeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efa73.trackee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
